80ac307b527980d0cd3c404f883154a8865e8284
RuslanLit <arslanlitvin@gmail.com>
2020-06-13 19:15:27 +0300

    added sell&buy section

--- app/js/main.js
+++ app/js/main.js
@@ -14,7 +14,7 @@ $(function () {
     $('.slider-weekly__slides').slick({
         nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/chevron-right.svg" alt="next arrow"></button>',
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.svg" alt="prev arrow"></button>',
-       
+        centerMode: false,
         infinite: true,
         appendArrows: '.arrows-wrap'
     });
@@ -24,7 +24,10 @@ $(function () {
         prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/chevron-left.svg" alt="prev arrow"></button>',
         appendArrows: '.folowers_arrows-wrap',
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        centerMode: false,
+        variableWidth: true,
+        infinite: true
     });
 
     var mixer = mixitup('.products__inner-box');
