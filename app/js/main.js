b413b34069cf64586349377502ff594fe656041a
RuslanLit <arslanlitvin@gmail.com>
2020-07-31 20:38:45 +0300

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -30,6 +30,17 @@ $(function () {
         infinite: true
     });
 
+    $('.testimonial__slider').slick({
+        nextArrow: '<button class="slick-arrow slick-next"><img src="images/chevron-left-slide.svg" alt="next arrow"></button>',
+        prevArrow: '<button class="slick-arrow slick-prev"><img src="images/chevronlslide.svg" alt="prev arrow"></button>',
+        centerMode: false,
+        fade: true,
+        variableWidth: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+    });
+
     var mixer = mixitup('.products__inner-box');
 
 });
